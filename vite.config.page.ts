adb2207da1283e5cac0733b62c7c3dde848d38be
Christoph Herb <ch.herb@gmx.de>
2024-03-05 01:42:38 +0100

    try to fix gh page

--- vite.config.page.ts
+++ vite.config.page.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
-import { extname, relative, resolve } from 'path'
-import { fileURLToPath } from 'node:url'
-import { glob } from 'glob'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     dts({ include: ['lib'] })
